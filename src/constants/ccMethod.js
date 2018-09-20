cedad4b98a7c4ebd8756110667003ed8411e9f85
clearives <clearives@gmail.com>
2018-09-20 17:45:14 +0800

    webview html js交互

---
+++ src/constants/ccMethod.js
@@ -0,0 +1,20 @@
+var ccMethod = {
+        method1: function () {
+            alert('method1')
+        },
+        method2: function () {
+            alert('method2')
+        }
+}
+const ccMethodStr = `
+    var ccMethod = {
+            method1: function () {
+                alert('method1')
+            },
+            method2: function () {
+                alert('method2')
+            }
+    }
+`
+
+export default ccMethodStr
