86d774ed1ade020b2d94d2d88b5f4c2c1a14bbfd
clearives <clearives@gmail.com>
2018-09-25 15:10:30 +0800

    工具类utils方法

--- src/container/home/HomeScreen.js
+++ src/container/home/HomeScreen.js
@@ -1,23 +1,64 @@
 import React, {Component} from 'react';
-import {Text, View} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
+import AppUtils from '../../utils/AppUtils'
 
 export default class HomeScreen extends Component {
-    static navigationOptions = {
-        header:null,  //隐藏顶部导航栏
-    };
     constructor(props) {
-        super(props);
+        super(props)
+        this.state = {
+            screenWidth: 0,
+            ScreenHeight: 0,
+            statusBarHeight: 0,
+            navBarHeight: 0,
+            tabBarHeight: 0,
+            bottomMargin: 0
+        }
     }
+
+    componentDidMount() {
+        this.setState({
+            screenWidth: AppUtils.getScreenWidth(),
+            ScreenHeight: AppUtils.getScreenHeight(),
+            statusBarHeight: AppUtils.getStatusBarHeight(),
+            navBarHeight: AppUtils.getNavBarHeight(),
+            tabBarHeight: AppUtils.getTabBarHeight(),
+            bottomMargin: AppUtils.getBottomMargin()
+        })
+    }
+
     goGalleryIndex = () => {
-        this.props.navigation.push('Gallery', { transition: 'forInitial' });
+        this.props.navigation.push('Gallery', {transition: 'forInitial'});
     }
+
     render() {
         return (
-            <View style={{flex: 1, backgroundColor: '#f2a0ff', justifyContent: 'center', alignItems: 'center'}}>
-                <Text style={{fontSize: 50}} onPress={() => this.goGalleryIndex()}>
-                    Gallery
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    AppUtils
                 </Text>
+                <View style={{paddingLeft: 60,paddingRight: 60}}>
+                    <Text style={{fontSize: 20}}>screenWidth: {this.state.screenWidth}</Text>
+                    <Text style={{fontSize: 20}}>ScreenHeight: {this.state.ScreenHeight}</Text>
+                    <Text style={{fontSize: 20}}>statusBarHeight: {this.state.statusBarHeight}</Text>
+                    <Text style={{fontSize: 20}}>navBarHeight: {this.state.navBarHeight}</Text>
+                    <Text style={{fontSize: 20}}>tabBarHeight: {this.state.tabBarHeight}</Text>
+                    <Text style={{fontSize: 20}}>bottomMargin: {this.state.bottomMargin}</Text>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        textAlign: 'center',
+        backgroundColor: '#fff'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 30,
+        marginTop: 30,
+        marginBottom: 30
+    }
+});
