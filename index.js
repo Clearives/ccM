b19a6a398c300ef2d838a61017fd3fff017b3a90
clearives <clearives@gmail.com>
2018-11-08 13:33:02 +0800

    全局storage

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 import {AppRegistry, YellowBox} from 'react-native';
 import App from './src/root';
+import {storage} from 'src/utils/storage/index';
 
+global.storage = storage;
 AppRegistry.registerComponent('Main', () => App);
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated']);
